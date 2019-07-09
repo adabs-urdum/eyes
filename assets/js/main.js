bb3963effd0b1cc9150ebee80efc88dc4528bc3a
Adabs Urdum <cyrill@adabs.ch>
2019-07-09 22:17:34 +0200

    built class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,99 @@
 'use strict';
 
-function Observer(){
+function Observer(initials){
 
   const observer = document.getElementsByClassName('observer')[0];
-  const sclera = observer.getElementsByClassName('observer__sclera')[0];
-  const iris = sclera.getElementsByClassName('observer__iris')[0];
-  const pupil = iris.getElementsByClassName('observer__pupil')[0];
-  const maxMoveIris = 55;
-  const maxMovePupil = 15;
-  const maxScaleIris = 0.13;
+  const eyes = observer.getElementsByClassName('observer__sclera');
+  const maxMoveIris = initials.maxMoveIris;
+  const maxMovePupil = initials.maxMovePupil;
+  const maxScaleIris = initials.maxScaleIris;
+  const threshold = initials.threshold;
 
-  let mouseX,
-      mouseY,
-      eyeCenterX,
-      eyeCenterY,
-      difference,
-      differencePercentage,
-      windowWidth,
-      windowHeight;
+  function Eye(sclera, i){
+
+    this.constructor = (sclera, i) => {
+      this.sclera = sclera;
+      this.iris = sclera.getElementsByClassName('observer__iris')[0];
+      this.pupil = this.iris.getElementsByClassName('observer__pupil')[0];
+
+      this.getEyePosition();
+      this.bindEvents();
+    }
+
+    this.bindEvents = () => {
+      window.addEventListener('mousemove', this.handleMouseMove);
+    }
+
+    this.getMousePosition = (e) => {
+      this.difference = {
+        x: e.clientX - this.eyeCenterX,
+        y: e.clientY - this.eyeCenterY
+      }
+
+      this.differencePercentage = {
+        x: this.difference.x,
+        y: this.difference.y,
+      };
+
+    }
+
+    this.handleMouseMove = (e) => {
+      this.getMousePosition(e);
+      this.setIrisOffset();
+      this.setPupilOffset();
+    }
+
+    this.setPupilOffset = () => {
+      let offsetX = maxMovePupil / 100 * this.differencePercentage.x - 50;
+      let offsetY = maxMovePupil / 100 * this.differencePercentage.y - 50;
+
+      this.pupil.style.transform = 'translate(' + offsetX + '%, ' + offsetY + '%)';
+    }
+
+    this.setIrisOffset = () => {
+      let offsetX = maxMoveIris / 100 * this.differencePercentage.x;
+      let offsetY = maxMoveIris / 100 * this.differencePercentage.y;
+      const translate = 'translate(' + offsetX + '%, ' + offsetY + '%)';
+      let scaleX = maxScaleIris / 100 * this.differencePercentage.x > 0 ? 1 - maxScaleIris / 100 * this.differencePercentage.x : 1 - maxScaleIris / 100 * this.differencePercentage.x * -1;
+      let scaleY = maxScaleIris / 100 * this.differencePercentage.y > 0 ? 1 - maxScaleIris / 100 * this.differencePercentage.y : 1 - maxScaleIris / 100 * this.differencePercentage.y * -1;
+      let scale = ' scaleX(' + scaleX + ')';
+      scale += ' scaleY(' + scaleY + ')';
+      this.iris.style.transform = translate + scale;
+    }
+
+    this.getEyePosition = () => {
+      const eyeBox = this.sclera.getBoundingClientRect();
+      this.eyeCenterX = eyeBox.left + eyeBox.width / 2;
+      this.eyeCenterY = eyeBox.top + eyeBox.height / 2;
+    }
+
+    this.constructor(sclera);
+
+  }
+
+  let windowWidth,
+      windowHeight,
+      i;
 
   function init(){
     setVars();
     bindEvents();
+    let i = 0;
+    for (let sclera of eyes) {
+      const eye = new Eye(sclera, i);
+      i += 1;
+    }
   }
 
   function setVars(){
-    getEyePosition();
     getWindowSize();
   }
 
   function bindEvents(){
-    observer.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('resize', handleWindowResize);
   }
 
   function handleWindowResize(){
-    getEyePosition();
     getWindowSize();
   }
 
@@ -44,51 +102,13 @@ function Observer(){
     windowHeight = window.innerHeight;
   }
 
-  function handleMouseMove(e){
-    getMousePosition(e);
-    setIrisOffset();
-    setPupilOffset();
-  }
-
-  function setPupilOffset(){
-    const offsetX = maxMovePupil / 100 * differencePercentage.x - 50;
-    const offsetY = maxMovePupil / 100 * differencePercentage.y - 50;
-    pupil.style.transform = 'translate(' + offsetX + '%, ' + offsetY + '%)';
-  }
-
-  function setIrisOffset(){
-    const offsetX = maxMoveIris / 100 * differencePercentage.x - 50;
-    const offsetY = maxMoveIris / 100 * differencePercentage.y - 50;
-    const translate = 'translate(' + offsetX + '%, ' + offsetY + '%)';
-    const scaleX = maxScaleIris / 100 * differencePercentage.x > 0 ? 1 - maxScaleIris / 100 * differencePercentage.x : 1 - maxScaleIris / 100 * differencePercentage.x * -1;
-    const scaleY = maxScaleIris / 100 * differencePercentage.y > 0 ? 1 - maxScaleIris / 100 * differencePercentage.y : 1 - maxScaleIris / 100 * differencePercentage.y * -1;
-    let scale = ' scaleX(' + scaleX + ')';
-    scale += ' scaleY(' + scaleY + ')';
-    iris.style.transform = translate + scale;
-  }
-
-  function getMousePosition(e){
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-    difference = {
-      x: mouseX - eyeCenterX,
-      y: mouseY - eyeCenterY
-    }
-
-    differencePercentage = {
-      x: 100 / eyeCenterX * difference.x,
-      y: 100 / eyeCenterY * difference.y,
-    };
-  }
-
-  function getEyePosition(){
-    const eyeBox = sclera.getBoundingClientRect();
-    eyeCenterX = eyeBox.left + eyeBox.width / 2;
-    eyeCenterY = eyeBox.top + eyeBox.height / 2;
-  }
-
   init();
 
 }
 
-Observer();
+Observer({
+  maxMoveIris: 55,
+  maxMovePupil: 15,
+  maxScaleIris: 0.13,
+  threshold: 100,
+});
